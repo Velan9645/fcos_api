5702b9df43077564c64a5573d8db1d00e60af708
bvdevelopers <bvdevelopers9645@gmail.com>
2024-10-07 12:07:39 +0530

    Update count.php

--- count.php
+++ count.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 error_reporting(E_ALL);
