436baac627f980e7826f7fad8f4869f3892731e8
bvdevelopers <bvdevelopers9645@gmail.com>
2024-11-10 11:30:28 +0530

    Create user.php

---
+++ user.php
@@ -0,0 +1,39 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Allow-Headers: Content-Type,Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+header('Content-Type: application/json');
+
+$servername = "165.232.184.202:15644";
+$username = "avnadmin";
+$password = "AVNS_L8JRTMLvztKOKDjzZTT";
+$dbname = "hari_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Connection failed: " . $conn->connect_error]));
+}
+
+// Total count
+$sql = "SELECT * FROM user";
+$result = $conn->query($sql);
+$totalcount = array();
+if ($result) {
+    $total = $result->fetch_assoc();
+} else {
+    $data = ["message" => "Query error: " . $conn->error];
+}
+
+$data = $total
+
+echo json_encode($data);
+
+$conn->close();
+?>
