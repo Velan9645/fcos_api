0720f95dbd3de3072a829a20f4a93f6d439e5820
bvdevelopers <bvdevelopers9645@gmail.com>
2024-12-21 19:13:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 
 header('Content-Type: application/json');
 
-$servername = "165.232.184.202:15644";
+$servername = "159.65.146.129:15644";
 $username = "avnadmin";
 $password = "AVNS_L8JRTMLvztKOKDjzZTT";
 $dbname = "if0_37120445_fcos";
